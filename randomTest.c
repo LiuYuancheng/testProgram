0c20350e4567a41cc9e2d33dfb1d55a41b893616
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-06-12 10:43:19 +0800

    Linear congruential generator[https://rosettacode.org/wiki/Linear_congruential_generator]

---
+++ randomTest.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+/* always assuming int is at least 32 bits */
+int rand();
+int rseed = 0;
+ 
+inline void srand(int x)
+{
+	rseed = x;
+}
+
+#define RAND_MAX ((1U << 31) - 1)
+ 
+inline int rand()
+{
+	return rseed = (rseed * 1103515245 + 12345) & RAND_MAX;
+}
+
+int main()
+{
+	int i;
+	printf("rand max is %d\n", RAND_MAX);
+    srand(0);
+	for (i = 0; i < 6; i++){
+        int randomN = rand()%128000+1;
+		printf("%d\n", randomN);
+    }
+	return 0;
+}
