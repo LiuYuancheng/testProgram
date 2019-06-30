c7946ba156001b0d20ddb74623fe9f83b8e1ff0a
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-06-30 22:04:54 +0800

    Add files via upload

--- Policy.py
+++ Policy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-@Author <LiuYuancheng/A0159354X>
+@Author <LiuYuancheng/A>
 Date :
 """
 
@@ -10,8 +10,6 @@ import httplib
 import json
 import os
 import subprocess
-import sys
-
 
 class flowStat(object):
     def __init__(self, server):
@@ -73,56 +71,122 @@ stat =  flowStat('127.0.0.1')
 
 # To insert the policies for the traffic applicable to path between S1 and S2
 def H1toH2():
-    #Limit the data flow radar between H1 and H2 to 1Mbps
-    # limit S1 to eth2
-    subprocess.Popen("ovs-vsctl -- set port S1-eth2 other-config:max-rate=1000000", shell=True)
-    # limit S2 to eth2
-    subprocess.Popen("ovs-vsctl -- set port S2-eth2 other-config:max-rate=1000000", shell=True)
+    subprocess.Popen( "ovs-vsctl add-br s1",shell=True)
+    subprocess.Popen( "ovs-vsctl add-port s eth0", shell=True)
+    subprocess.Popen("ovs-vsctl set port eth1 qos=@newqos -- --id=@newqos create qos \
+                    type=linux-htb other-config:max-rate=5000000 queues:0=@newqueue -- \
+                    --id=@newqueue create queue other-config:min-rate=1000000 \
+                    other-config:max-rate=1000000", shell=True)
+    # Set the speed limitation on S1
+    subprocess.Popen( "ovs-vsctl add-br s2",shell=True)
+    subprocess.Popen( "ovs-vsctl add-port s2 eth0", shell=True)
+    subprocess.Popen("ovs-vsctl set port eth1 qos=@newqos -- --id=@newqos create qos \
+                    type=linux-htb other-config:max-rate=5000000 queues:0=@newqueue -- \
+                    --id=@newqueue create queue other-config:min-rate=1000000 \
+                    other-config:max-rate=1000000", shell=True)
+
+#    subprocess.Popen( "sudo ovs-vsctl %s -- --id=@defaultqos create qos type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,1=@q1,2=@q2 -- --id=@q0 create queue other-config:max-rate=1000000000  other-config:min-rate=1000000000 -- --id=@q1 create queue other-config:max-rate=1000000 other-config:min-rate=1000000 -- --id=@q2 create queue other-config:max-rate=512000 other-config:min-rate=512000",shell=True)
+
     pass
+
 
 # To insert the policies for the traffic applicable to path between S2 and S3
 def H2toH3():
-    #Policy 1: Block all traffic using UDP port from 1000-1100 with high priority = 100
     for i in range(1000,1100):
         port = str(i)
-        S2toS3UDPBlock = {'switch': "00:00:00:00:00:00:00:02", "name": "S2h2toh3UDP", "cookie": "0",
-                            "priority": "100", "in_port": "1", "eth_type": "0x800", 'src-ip': "10.0.0.2/32",
-                            "dst-ip": "10.0.0.3", "nw-proto": "UDP","tp-src": port, "priority": "10", "action": "DENY"}
-        S3toS2UDPBlock = {'switch': "00:00:00:00:00:00:00:03", "name": "S3h3toh2UDP", "cookie": "0",
-                            "priority": "100", "in_port": "1", "eth_type": "0x800", 'src-ip': "10.0.0.3/32",
-                            "dst-ip": "10.0.0.2", "nw-proto": "UDP","tp-src": port, "priority": "10", "action": "DENY"}
+        S2toS3UDPBlock = {'src-ip': "10.0.0.2/32", "dst-ip": "10.0.0.3", "nw-proto": "UDP","tp-src": port, "priority": "10", "action": "DENY"}
+        S3toS2UDPBlock = {'src-ip': "10.0.0.3/32", "dst-ip": "10.0.0.2", "nw-proto": "UDP","tp-src": port, "priority": "10", "action": "DENY"}
         pusher.set(S2toS3UDPBlock)
-        pusher.set(S3toS2UDPBlock)
-        # Add the firewall DENY rule also
-        subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.2/32\", \"nw-proto\":\"UDP\",tp-src\":\""+port+"\",\"priority\":\"100\", \"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
-        subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.2/32\", \"nw-proto\":\"UDP\",tp-src\":\""+port+"\",\"priority\":\"100\",\"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
-        subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.3/32\", \"nw-proto\":\"UDP\",tp-src\":\""+port+"\",\"priority\":\"100\", \"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
-        subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.3/32\", \"nw-proto\":\"UDP\",tp-src\":\""+port+"\",\"priority\":\"100\", \"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
+        pusher.set(S2toS3UDPBlock)
+    # block all UDP from 1000 to 1100
+
+
+
+
+    # allow UDP communication
+    #subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.2/32\", \"dl-type\":\"ARP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    #subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.2/32\", \"dl-type\":\"ARP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    #subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.3/32\", \"dl-type\":\"ARP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    #subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.3/32\", \"dl-type\":\"ARP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+
+    #subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.2/32\", \"nw-proto\":\"UDP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    #subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.2/32\", \"nw-proto\":\"UDP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    #subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.3/32\", \"nw-proto\":\"UDP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    #subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.3/32\", \"nw-proto\":\"UDP\" }' http://localhost:8080/wm/firewall/rules/json",shell=True)
+    # block port 1000 to 1100
+    #for i in range(1000, 1100):
+    #    subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.2/32\", \"nw-proto\":\"UDP\",tp-src\":\""+str(i)+"\",\"priority\":\"100\", \"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
+    #    subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.2/32\", \"nw-proto\":\"UDP\",tp-src\":\""+str(i)+"\",\"priority\":\"100\",\"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
+    #    subprocess.Popen("curl -X POST -d '{\"src-ip\": \"10.0.0.3/32\", \"nw-proto\":\"UDP\",tp-src\":\""+str(i)+"\",\"priority\":\"100\", \"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
+    #    subprocess.Popen("curl -X POST -d '{\"dst-ip\": \"10.0.0.3/32\", \"nw-proto\":\"UDP\",tp-src\":\""+str(i)+"\",\"priority\":\"100\", \"action\":\"DENY\" }' http://localhost:8080/wm/firewall/rules/json | python -m json.tool",shell=True)
+    pass
+# To insert the policies for the traffic applicable to path between S1 and S3
+
+def find_all(a_str, sub_str):
+    start = 0
+    b_starts = []
+    while True:
+        start = a_str.find(sub_str, start)
+        if start == -1: return b_starts
+        #print start
+        b_starts.append(start)
+        start += 1
+
+def H1toH3():
+    cmd = "ovs-vsctl show"
+    p = os.popen(cmd).read()
+    # print p
+    brdgs = find_all(p, "Bridge")
+
+    switches = []
+    for bn in brdgs:
+        sw = p[(bn + 8):(bn + 10)]
+        switches.append(sw)
+
+    ports = find_all(p, "Port")
+    # print ports
+    prts = []
+    for prt in ports:
+        prt = p[(prt + 6):(prt + 13)]
+        if '"' not in prt:
+            # print prt
+            prts.append(prt)
+    config_strings = {}
+    for i in range(len(switches)):
+        str = ""
+        sw = switches[i]
+        for n in range(len(prts)):
+            # verify correct order
+            if switches[i] in prts[n]:
+                # print switches[i]
+                # print prts[n]
+                port_name = prts[n]
+                str = str + " -- set port %s qos=@defaultqos" % port_name
+        config_strings[sw] = str
+    flow_size = 0
+    print "-------------------"
+    while True:
+        pusher_flowS1= stat.get("S1")
+        pusher_flowS3= stat.get('S3')
+	print len(pusher_flowS1)
+	print len(pusher_flowS3)
+
+        if len(pusher_flowS1) == len(pusher_flowS3):
+        # if the data len is same, that means the flow is between S1 and S3
+            flow_size+= len(pusher_flowS1)
+            #if flow_size*8 < 20000000:
+             #   queuecmd = "sudo ovs-vsctl %s -- --id=@defaultqos create qos type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,1=@q1,-- --id=@q0 create queue other-config:max-rate=1000000000  other-config:min-rate=1000000000 -- --id=@q1 create queue other-config:max-rate=1000000 other-config:min-rate=1000000" % config_strings['S1']
+              #  os.popen(queuecmd).read()
+              #  queuecmd = "sudo ovs-vsctl %s -- --id=@defaultqos create qos type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,1=@q1, -- --id=@q0 create queue other-config:max-rate=1000000000  other-config:min-rate=1000000000 -- --id=@q1 create queue other-config:max-rate=1000000 other-config:min-rate=1000000" % config_strings['S3']
+               # os.popen(queuecmd).read()
+            #if flow_size*8 >= 20000000 and flow_size*8 < 30000000:
+            #    queuecmd = "sudo ovs-vsctl %s -- --id=@defaultqos create qos type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,2=@q2 -- --id=@q0 create queue other-config:max-rate=1000000000  other-config:min-rate=1000000000  -- --id=@q2 create queue other-config:max-rate=512000 other-config:min-rate=512000" % config_strings['S1']
+            #    os.popen(queuecmd).read()
+             #   queuecmd = "sudo ovs-vsctl %s -- --id=@defaultqos create qos type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,2=@q2 -- --id=@q0 create queue other-config:max-rate=1000000000  other-config:min-rate=1000000000  -- --id=@q2 create queue other-config:max-rate=512000 other-config:min-rate=512000" % config_strings['S3']
+              #  os.popen(queuecmd).read()
+
     pass
 
-# To insert the policies for the traffic applicable to path between S1 and S3
-def H1toH3():
-    switch_1 = '00:00:00:00:00:01'
-    switch_3 = '00:00:00:00:00:03'
-    flow_size = 0
-    while True:
-        pusher_flowS1= stat.get(switch_1)
-        pusher_flowS3= stat.get(switch_3)
-        if sys.getsizeof(pusher_flowS1) == sys.getsizeof(pusher_flowS3):
-        # if the data flow byte size is same, that means the flow is between S1 and S3
-            flow_size+= pusher_flowS1["byteCount"]
-            if flow_size*8 < 20000000:
-                queuecmd = "ovs-vsctl -- set port S1-eth1 other-config:max-rate=1000000"
-                os.popen(queuecmd).read()
-                queuecmd = "ovs-vsctl -- set port S3-eth1 other-config:max-rate=1000000"
-                os.popen(queuecmd).read()
-            if flow_size*8 >= 20000000 and flow_size*8 < 30000000:
-                queuecmd = "ovs-vsctl -- set port S1-eth1 other-config:max-rate=512000"
-                os.popen(queuecmd).read()
-                queuecmd = "ovs-vsctl -- set port S3-eth1 other-config:max-rate=512000"
-                os.popen(queuecmd).read()
-            else:
-                return
 
 def staticForwarding():
     # Below 4 flows are for setting up the static forwarding for the path H1->S1->S2->H2 & vice-versa
@@ -195,7 +259,7 @@ if __name__ =='__main__':
     # Starting the basic Forwarding/routing of packets b/w the three hosts
     staticForwarding()
     # Implementing the organizational policy, for communication path specified
-    H1toH2()
-    H2toH3()
+    #H1toH2()
+    #H2toH3()
     H1toH3()
     pass
